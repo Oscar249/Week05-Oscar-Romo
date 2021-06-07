b62b303c08f5032ff099fe39efc89c539e6c592b
Oscar249 <80767588+Oscar249@users.noreply.github.com>
2021-06-07 16:26:41 -0500

    Create ex03-Oscar Romo.c

---
+++ ex03-Oscar Romo.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h> // input output data
+#include <math.h> 
+#include <string.h> // string handling operations
+#define cm  60
+#define ch  3600
+
+int main(void) {
+float res;
+res = (4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19);
+printf("el resultado de la operacion es %f \n", res);
+
+}
